e49174819e099576c0c944278c5fe26382328f94
BadBoy-Github <elayabarathiedison@gmail.com>
2026-03-29 18:37:49 +0530

    footer fix.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import { FaWhatsapp } from "react-icons/fa";
 // Sitemap links data - easy to edit
 const sitemapLinks = [
   { name: "Home", href: "/" },
-  { name: "Why Us?", href: "/#about" },
-  { name: "Featured", href: "/featured" },
+  { name: "Why Us?", href: "/#featured" },
+  { name: "Featured", href: "/#featured-gift-cards" },
   { name: "Gift Cards", href: "/#gift-cards" },
   { name: "Wishlist", href: "/wishlist" },
   { name: "Cart", href: "/cart" },
